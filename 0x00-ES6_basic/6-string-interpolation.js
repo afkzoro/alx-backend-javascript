c38e3f2a893382f1803f5ba73dc2adec389326e2
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-04-22 04:33:24 +0100

    commit budget

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,9 +1,9 @@
 export default function getSanFranciscoDescription() {
   const year = 2017;
   const budget = {
-    income: "$119868",
-    gdp: "$154.2 billion",
-    capita: "$178,479",
+    income: '$119868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
   };
 
   return `As of ${year}, it was the seventh-highest
@@ -12,3 +12,5 @@ export default function getSanFranciscoDescription() {
    As of 2015, San Francisco proper had a GDP of ${budget.gdp}
     , and a GDP per capita of ${budget.capita}.`;
 }
+
+As of 2017, it was the seventh-highest income county in the United States, with a per capita personal income of $119,868. As of 2015, San Francisco proper had a GDP of $154.2 billion, and a GDP per capita of $178,479.

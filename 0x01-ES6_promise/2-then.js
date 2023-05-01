24bdb202ff3208c2b24b38b449984a28ae79dbf7
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-01 19:00:04 +0100

    commit promise

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export default function handleResponseFromAPI(promise) {
+  promise.then((data) => {
+    console.log('Got a response from the API');
+    return {
+      status: 200,
+      body: 'success',
+    };
+  }).catch((error) => {
+    console.error(error);
+    return new Error();
+  });
+}

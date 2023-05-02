2da3dd442192c772751ab7d4d00049e8bfb6aaaf
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-02 16:47:50 +0100

    fix import

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import signUpUser from './4-user-promise';
-import { uploadPhoto } from './utils';
+import { uploadPhoto } from '5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promisesList = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,9 +1,10 @@
+/* eslint-disable */
 export default function loadBalancer(chinaDownload, USDownload) {
- return Promise.race([chinaDownload, USDownload]) /* 
-   .then((result) => {
-     return result;
-   })
-   .catch((error) => {
-     console.log(error);
-   });
+  return Promise.race([chinaDownload, USDownload]) /* Execcutes both promises in parallel */
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }

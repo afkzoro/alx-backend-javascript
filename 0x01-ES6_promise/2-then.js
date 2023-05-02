30e9d7820faa3f45acf0a359ff51050e5307fcbb
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-02 03:05:23 +0100

    fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,4 @@
-/* eslint-disable */
 export default function handleResponseFromAPI(promise) {
-  promise.then((data) => {
-    console.log('Got a response from the API');
-    return {
-      status: 200,
-      body: 'success'
-    };
-  }).catch((error) => {
-    console.error(error);
-    return new Error();
-  });
+  return promise.then(() => ({ status: 200, body: 'success' })).catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }

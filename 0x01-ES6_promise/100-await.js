783c63311b31daf9abdb19409df14a0323dd4ce9
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-02 17:14:30 +0100

    async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return {
+      photo: photo,
+      user: user,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}

426ba0c319225fa36af2ae001d27b99a967072d7
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-03 03:17:34 +0100

    fix lint

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,6 @@
+/* eslint-disable */
 export default class ClassRoom {
   constructor(maxStudentsSize) {
-    this.maxStudentsSize = maxStudentsSize;
+    this._maxStudentsSize = maxStudentsSize;
   }
 }

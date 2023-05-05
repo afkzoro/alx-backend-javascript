763fac274789f789992bd6f2d6d60ac494d9e2ad
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-05 04:20:09 +0100

    commit

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -17,7 +17,3 @@ export default class Airport {
     return `[object ${this._code}]`;
   }
 }
-
-const airportSF = new Airport('San Francisco Airport', 'SFO');
-console.log(airportSF);
-console.log(airportSF.toString());

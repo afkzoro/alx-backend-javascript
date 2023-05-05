44d271b9b48f7210655c88444011139c5b2e6b25
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-05 11:15:04 +0100

    commit

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export default function getListStudentIds(studentsList) {
+ if (!Array.isArray(studentsList)) {
+   return [];
+ }
+ return studentsList.map(student => student.id);
+}

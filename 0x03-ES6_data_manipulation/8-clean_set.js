3f916bca50b3681f8310871fda515044c6db368b
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-06 10:21:51 +0100

    #8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default function cleanSet(set, startString) {
+  let result = '';
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result += value.slice(startString.length) + ',';
+    }
+  });
+  return result.slice(0, -1);
+}

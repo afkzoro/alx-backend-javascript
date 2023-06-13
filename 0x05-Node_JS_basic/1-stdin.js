c89e1f4cdfc62244f6641105e2e235b40c9ddde4
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-06-13 12:55:05 +0100

    Handle input piped through cmd

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,14 +6,23 @@ const rl = readline.createInterface({
 });
 
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  if (name === 'exit') {
+  if (name.trim().toLowerCase() === 'exit') {
     rl.close();
     process.stdout.write('This important software is now closing\n');
     process.exit();
   } else {
     process.stdout.write(`Your name is: ${name}\n`);
     rl.close();
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
   }
 });
+
+// Handle input piped through the command line
+if (!process.stdin.isTTY) {
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('data', (data) => {
+    const name = data.trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  });
+}

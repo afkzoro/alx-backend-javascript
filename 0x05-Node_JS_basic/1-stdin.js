2917736d8eee823e1f1f20b02f050a64d7423b94
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-06-13 12:58:14 +0100

    FIx edge case

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,23 +6,14 @@ const rl = readline.createInterface({
 });
 
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  if (name.trim().toLowerCase() === 'exit') {
+  if (name === 'exit') {
     rl.close();
     process.stdout.write('This important software is now closing\n');
     process.exit();
   } else {
-    process.stdout.write(`Your name is: ${name}`);
-    rl.close();
-  }
-});
-
-// Handle input piped through the command line
-if (!process.stdin.isTTY) {
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', (data) => {
-    const name = data.trim();
     process.stdout.write(`Your name is: ${name}\n`);
+    rl.close();
     process.stdout.write('This important software is now closing\n');
     process.exit();
-  });
-}
+  }
+});

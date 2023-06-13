ab43049454972dcdd9ddab2c74aef9e6b37ab19c
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-06-13 12:49:58 +0100

    fix ouput

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,10 +7,13 @@ const rl = readline.createInterface({
 
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   if (name === 'exit') {
+    rl.close();
     process.stdout.write('This important software is now closing\n');
-    rl.close();
+    process.exit();
   } else {
-    process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}\n`);
     rl.close();
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
   }
 });

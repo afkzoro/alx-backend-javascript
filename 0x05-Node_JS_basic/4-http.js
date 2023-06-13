967a8f1b8938dafa14a43545fb2c16241a9a98b7
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-06-13 13:31:52 +0100

    create http server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+const http = require("http");
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello Holberton School!\n");
+});
+
+app.listen(1245, () => {
+  console.log("Server is running on http://localhost:1245");
+});
+
+module.exports = app;

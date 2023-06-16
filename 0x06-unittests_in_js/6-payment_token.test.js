cbf09cfb3eb670468d0d0d8d4bf32ac6aef769d3
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-06-16 12:28:49 +0100

    skip test and fix function

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,11 +1,12 @@
 /* eslint-disable */
+const expect = require('chai').expect;
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
   it('should return a resolved promise with the correct data when success is true', (done) => {
     getPaymentTokenFromAPI(true)
       .then(response => {
-        expect(response).toEqual({ data: 'Successful response from the API' });
+        expect(response).to.deep.equal({ data: 'Successful response from the API' });
         done();
       })
       .catch(error => {

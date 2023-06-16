ad26b9c6fa71356522f07bc0977d50bbc97a1073
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-06-16 12:20:35 +0100

    Task #6 and #7

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a resolved promise with the correct data when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then(response => {
+        expect(response).toEqual({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch(error => {
+        done(error);
+      });
+  });
+});
